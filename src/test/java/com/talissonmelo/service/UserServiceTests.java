61bd9f46fc8a1d5a0e26d99ee86127af12c7db37
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-06-01 09:38:08 -0300

    Testando Método de Logon de Usuário

--- src/test/java/com/talissonmelo/service/UserServiceTests.java
+++ src/test/java/com/talissonmelo/service/UserServiceTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.talissonmelo.entity.User;
+import com.talissonmelo.entity.dto.UserDTO;
 import com.talissonmelo.repository.UserRepository;
 import com.talissonmelo.service.exception.RuleException;
 
@@ -41,5 +43,22 @@ public class UserServiceTests {
 		// Ação - Execução
 		Assertions.assertThrows(RuleException.class, () -> service.existEmail("talis@gmail.com"));
 	}
-
+	
+	@Test
+	public void logonAuthenticateUser() {
+		// Cenário
+		String email = "talis@gmail.com";
+		String password = "12345";
+		
+		User user = User.builder().id(1l).name("Talisson").email(email).password(password).build();
+		Mockito.when(repository.findByEmail(email)).thenReturn(user);
+		UserDTO dto = new UserDTO(email, password);
+		
+		// Ação - Execução
+		User result = service.logonEmailPassword(dto);
+		
+		//Verificação
+		Assertions.assertNotNull(result);
+		
+	}
 }
